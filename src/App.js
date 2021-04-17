d3c2b5ba9bff8359b2cde5b7f7856203e9470128
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-04-17 10:14:43 -0400

    exercise one complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Counter from "./Components/Counter";
+import Switch from "./Components/Switch"
 
 function App() {
   return <div className="App">
     <h1>This is React</h1>
     <Counter />
+    <Switch />
+    <Switch />
+    <Switch />
   </div>;
 }
 
---
+++ src/Components/Switch.js
@@ -0,0 +1,16 @@
+import "./Switch.css"
+
+function Switch() {
+    return (
+        <div className="Switch">
+            <h3>Switch</h3>
+            <p>OFF</p>
+            <p>
+                <button type="button">On</button>
+                <button type="button">Off</button>
+            </p>
+        </div>
+    )
+}
+
+export default Switch;
