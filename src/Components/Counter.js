271a32a0d4865806b64eee059fde46c55b28553b
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-04-17 10:48:15 -0400

    counter logic working

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import "./Counter.css";
 
 function Counter() {
+    const [ count, setCount] = useState(0);
+
+    function handleReset() {
+        setCount(0); // use setCount to modify the state at runtime
+    }
+
+    function handleUp() {
+        setCount(prevCount => prevCount + 1);
+    }
+
     return (
         <div className="Counter">
             <h3>Counter</h3>
-            <p>0</p>
+            <p>{count}</p>
             <p>
-                <button type="button">Down</button>
-                <button type="button">Reset</button>
-                <button type="button">Up</button>
+                <button type="button" onClick={() => setCount(prev => prev - 1)}>Down</button>
+                <button type="button" onClick={handleReset}>Reset</button>
+                <button type="button" onClick={handleUp}>Up</button>
             </p>
         </div>
     );
