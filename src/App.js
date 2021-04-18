dc2c552dcfced6cbd0aa5274e02c0362d0d116b0
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-04-18 09:34:50 -0400

    created new weather component and quotes component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import Counter from "./Components/Counter";
 import Switch from "./Components/Switch"
+import Weather from "./Components/Weather"
 
 function App() {
   return <div className="App">
     <h1>This is React</h1>
     <Counter />
     <Switch />
-    <Switch />
-    <Switch />
-  </div>;
+   <Weather time="Today" conditions="Overcast" tempF={45} />
+  </div>
 }
 
 export default App;
---
+++ src/Components/Votes.js
@@ -0,0 +1,7 @@
+// import { useState } from "react";
+
+// // function Votes () {
+// //     const [chocolate, setChocolate] = useState(0);
+// //     const [ vanilla, setVanilla] = useState(0);
+// //     const [strawberry, setStrawberry] = useState(0);
+// // }
